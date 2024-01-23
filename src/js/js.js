1fb2ef7ceca3de0092cd4047c8ddf472be64e8a7
Muhammad Darda Fathammubienaa <muhammad.darda@Huawae-MacBook-Pro.local>
2024-01-23 10:23:42 +0700

    second-commit

--- src/js/js.js
+++ src/js/js.js
@@ -2,11 +2,14 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const navbarFixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset){
-        header.classList.add('navbar-fixed')
+        header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
     }else{
-       header.classList.remove('navbar-fixed')
+       header.classList.remove('navbar-fixed');
+       toTop.classList.add('hidden');
     }
 }
 
@@ -17,4 +20,38 @@ const navMenu = document.querySelector('#nav-menu');
 humberger.addEventListener('click', function(){
     humberger.classList.toggle('active');
     navMenu.classList.toggle('hidden');
-})
+});
+
+// klik dimanapun
+window.addEventListener('click', function(a){
+    if(a.target !=humberger && a.target !=navMenu){
+        humberger.classList.remove('active');
+        navMenu.classList.add('hidden');
+
+    }
+});
+
+// dark mode
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// perpindahaan mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    darkToggle.checked = true;
+  } else {
+    document.documentElement.classList.remove('dark')
+    darkToggle.checked = false;
+  }
+  
+   
